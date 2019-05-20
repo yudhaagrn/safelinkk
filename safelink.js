14ed0bec251b46714564bef12166956835a1d102
yudhaagrn <50839637+yudhaagrn@users.noreply.github.com>
2019-05-21 06:28:39 +0700

    Create safelink.js

---
+++ safelink.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+//<![CDATA[
+var setting={exceptionurl:"mecha-logy.blogspot.com,facebook.com,google.com,twitter.com",path:"#?o="};
+//]]>
+</script>
+<script src="https://cdn.jsdelivr.net/gh/Indzign/theme@master/autosafelink.js"></script>
+<script src="https://www.blogger.com/feeds/7574947352267943194/posts/default?alt=json-in-script&max-results=150&callback=showurl"></script>
